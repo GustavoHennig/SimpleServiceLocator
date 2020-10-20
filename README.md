8f1bd6b138afb663c1ce7816c6c8d640984a7b4e
Gustavo Augusto Hennig <gustavohe@gmail.com>
2020-10-20 16:16:52 -0300

    Initial version

---
+++ README.md
@@ -0,0 +1,7 @@
+﻿
+### Simple Service Locator (C#)
+
+
+Very simple ServiceLocator implementation.
+
+An easy alternative to Dependency Injection for small or legacy projects.
---
+++ ServiceLocator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GHSoftware
+{
+    /// <summary>
+    /// Very simple ServiceLocator implementation, an easy alternative to Dependency Injection for small or legacy projects
+    /// </summary>
+    public class ServiceLocator
+    {
+        public static ServiceLocator Default = new ServiceLocator();
+
+        private readonly ConcurrentDictionary<Type, object> container = new ConcurrentDictionary<Type, object>();
+
+        public ServiceLocator()
+        {
+
+
+        }
+
+        public void Set<T>(T t)
+        {
+            container.TryAdd(typeof(T), t);
+        }
+
+        public T Get<T>()
+        {
+            try
+            {
+                Type t = typeof(T);
+                return (T)container.GetOrAdd(t, _ => (T)Activator.CreateInstance(typeof(T)));
+            }
+            catch (Exception ex)
+            {
+                throw new NotImplementedException("Service not available: " + ex.Message);
+            }
+        }
+    }
+}
